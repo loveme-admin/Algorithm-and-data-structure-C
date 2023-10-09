4d621e653acd3e676e4afca3635f682dacc4ac7b
loveme-admin <lovemepwp@gmail.com>
2023-10-09 22:41:45 +0800

    Update File

--- Data-Structure/Sqlist-Link-head/main.c
+++ Data-Structure/Sqlist-Link-head/main.c
@@ -5,7 +5,7 @@
 #include<time.h>
 
 void InitSqList(LNode **);
-void BuildSqList(LNode*);
+LNode * BuildSqList(LNode*);
 void InputSqList(LNode*);
 
 void InitSqList(LNode** head)
@@ -15,21 +15,23 @@ void InitSqList(LNode** head)
 	(*head)->data = 0;
 }
 
-void BuildLNode(LNode* head)
+LNode* BuildLNode(LNode* head)
 {
 	LNode *temp = head->next;
 	head->next = (LNode*)malloc(sizeof(LNode));
 	head->next->next = temp;
-	InputSqList(head->next);
+	head->data++;
+	return head->next;
 }
 
-void BuildLNodeAlter(LNode* head)
+LNode * BuildLNodeAlter(LNode* head)
 {
 	LNode* temp = head;
 	for (; temp->next != NULL; temp=temp->next);
 	temp->next= (LNode*)malloc(sizeof(LNode));
 	temp->next->next = NULL;
-	InputSqList(temp->next);
+	head->data++;
+	return temp->next;
 }
 
 void InputSqList(LNode* temp)
@@ -47,15 +49,35 @@ void PrintSqList(LNode* head)
 	}
 }
 
+void InsertLNode(LNode *head,int num)
+{
+	LNode* temp = head;
+	LNode* temp2, *temp3;
+	for (int i = 1; i < num; i++)
+		temp=temp->next;
+	temp2= (LNode*)malloc(sizeof(LNode));
+	InputSqList(temp2);
+	temp3=temp->next;
+	temp->next = temp2;
+	temp2->next = temp3;
+	head->data++;
+}
+
 int main(void)
 {
 	LNode* head = NULL;
+	LNode* temp = NULL;
 	ScanfQueue queue;
 	InitSqList(&head);
-	BuildLNode(head);
-	BuildLNodeAlter(head);
-	BuildLNodeAlter(head);
-	BuildLNode(head);
+	temp = BuildLNode(head);
+	InputSqList(temp);
+	temp=BuildLNodeAlter(head);
+	InputSqList(temp);
+	temp=BuildLNodeAlter(head);
+	InputSqList(temp);
+	temp=BuildLNode(head);
+	InputSqList(temp);
+	InsertLNode(head, 2);
 	PrintSqList(head);
 	return 0;
 }
