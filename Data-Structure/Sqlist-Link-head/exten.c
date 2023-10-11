f60858666203ea7fd0e73b495585575df795d0c7
loveme-admin <lovemepwp@gmail.com>
2023-10-11 21:20:53 +0800

    Update File

--- Data-Structure/Sqlist-Link-head/exten.c
+++ Data-Structure/Sqlist-Link-head/exten.c
@@ -52,7 +52,9 @@ Bool HelpList()
 	printf(" 1   初始线性表 2   销毁线性表\n");
 	printf(" 3   按位插入值 4   按位删除值\n");
 	printf(" 5   按值查找位 6   按位查找值\n");
+	printf(" 7   头插建立表 8   尾插建立表\n");
 	printf(" 101 打印线性表 102 随机初始化\n");
 	printf("-1   退出线性表 0   帮助线性表\n");
+	printf("-1   结束输入值\n");
 	return TRUE;
 }
--- Data-Structure/Sqlist-Link-head/main.c
+++ Data-Structure/Sqlist-Link-head/main.c
@@ -1,20 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"root.h"
-
-Bool InitList(LNode **);
-Bool DestoryList(LNode**);
-
-Bool InsertTailList(LNode*);
-Bool InsertHeadList(LNode*);
-Bool InsertElem(LNode*, int, EleType);
-Bool DeleteElem(LNode*, int, EleType);
-
-Bool PrintList(LNode *);
-Bool MenuList(LNode **,ScanfQueue *);
+//基本操作
+Bool InitList(LNode **);                //初始化线性表
+Bool DestoryList(LNode**);              //销毁线性表
+//常用操作
+Bool InsertTailList(LNode*);            //头插法建立线性表
+Bool InsertHeadList(LNode*);            //尾插法建立线性表
+Bool InsertElem(LNode*, int, EleType);  //按位插入元素
+Bool DeleteElem(LNode*, int);           //删除元素并返回值
+LNode* LocateElem(LNode*, EleType);     //按值查找元素
+LNode* GetElem(LNode*, int);            //按位查找元素
+//辅助操作
+Bool PrintList(LNode *);                //打印线性表
+Bool MenuList(LNode **,ScanfQueue *);   //菜单
 
 Bool InitList(LNode** head)
 {
+	if (head == NULL)
+		return FALSE;
 	(*head) = (LNode*)malloc(sizeof(LNode));
 	if (*head == NULL)
 		return FALSE;
@@ -25,7 +29,7 @@ Bool InitList(LNode** head)
 
 Bool DestroyList(LNode** head)
 {
-	if (*head == NULL)
+	if (head == NULL)
 		return FALSE;
 	LNode* temp = *head;
 	for (; temp != NULL; temp = *head)
@@ -52,6 +56,7 @@ Bool InsertHeadList(LNode* head)
 		temp->data = data;
 		temp->next = head->next;
 		head->next = temp;
+		head ->data++;
 		scanf("%d",&data);
 	}
 	return TRUE;
@@ -99,7 +104,7 @@ Bool InsertElem(LNode* head, int num, EleType data)
 	return TRUE;
 }
 
-Bool DeleteElem(LNode* head, int num, EleType data)
+Bool DeleteElem(LNode* head, int num)
 {
 	if (head == NULL)
 		return FALSE;
@@ -113,6 +118,28 @@ Bool DeleteElem(LNode* head, int num, EleType data)
 	free(temp);
 	head->data--;
 	return TRUE;
+}
+
+LNode* LocateElem(LNode* head, EleType data)
+{
+	if (head == NULL)
+		return FALSE;
+	LNode* temp = head->next;
+	for (; temp != NULL; temp = temp->next)
+		if (temp->data == data)
+			return temp;
+	return FALSE;
+}
+
+LNode* GetElem(LNode* head , int num)
+{
+	if (head == NULL)
+		return FALSE;
+	if (num<1 || num>head->data)
+		return FALSE;
+	LNode* temp = head;
+	for (int i = 1; i <= num; i++, temp = temp->next);
+	return temp;
 }
 
 Bool PrintList(LNode* head)
@@ -134,39 +161,48 @@ Bool MenuList(LNode** head, ScanfQueue* queue)
 		printf("Input Num:");
 		scanf("%d", &num);
 		switch (num) {
-		case 1: InitList(&head); break;
-		case 2: DestroyList(&head); break;
+		case 1: InitList(head); break;
+		case 2: DestroyList(head); break;
 		case 3:
 		{
 			printf("Input locate and data:");
 			ScanfPackage(queue, 2);
-			InsertElem(head, queue->data[0], queue->data[1]);
+			InsertElem(*head, queue->data[0], queue->data[1]);
 			break;
 		}
 		case 4:
 		{
 			printf("Input locate:");
 			ScanfPackage(queue, 1);
-			DeleteElem(head, queue->data[0], &(queue->data[0]));
+			DeleteElem(*head, queue->data[0]);
 			break;
 		}
 		case 5:
 		{
-			//printf("Input data:");
-			//ScanfPackage(queue, 1);
-			//printf("%d\n", LocateElem(*list, queue->data[0]));
-			//break;
+			printf("Input data:");
+			ScanfPackage(queue, 1);
+			printf("%p\n", LocateElem(*head, queue->data[0]));
+			break;
 		}
 		case 6:
 		{
-			//printf("Input locate:");
-			//ScanfPackage(queue, 1);
-			//printf("%d\n", GetElem(*list, queue->data[0]));
-			//break;
+			printf("Input locate:");
+			ScanfPackage(queue, 1);
+			printf("%d\n", (GetElem(*head, queue->data[0]))->data);
+			break;
 		}
-
-		case 101: PrintList(head); break;
-		case 102: InitRandomList(&head); break;
+		case 7:
+		{
+			InsertHeadList(*head);
+			break;
+		}
+		case 8:
+		{
+			InsertTailList(*head);
+			break;
+		}
+		case 101: PrintList(*head); break;
+		case 102: InitRandomList(head); break;
 		case 0: HelpList(); break;
 		case -1:return OK; break;
 		default:printf("Num Error!\n");
