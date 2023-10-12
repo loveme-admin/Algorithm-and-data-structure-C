c6789500a3a2d100c8567cb30b58832d454e04ed
loveme-admin <lovemepwp@gmail.com>
2023-10-12 22:42:49 +0800

    Update File

---
+++ Data-Structure/SqList-LoopLink-Head/exten.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include"root.h"
+
+//辅助操作
+Bool InitRandomList(LNode**);            //随机初始化
+Bool ScanfPackage(ScanfQueue*, int);    //输入队列
+Bool HelpList();                        //帮助菜单
+
+Bool InitRandomList(LNode** head)
+{
+	if (head == NULL)
+		return FALSE;
+	*head = (LNode*)malloc(sizeof(LNode));
+	if (*head == NULL)
+		return FALSE;
+	(*head)->data = 0;
+	(*head)->next =*head;
+	LNode* temp = NULL;
+	int count=0;
+	printf("Input Data Num:");
+	scanf("%d", &count);
+	srand((unsigned)time(NULL));
+	for (int i = 1; i <= count; i++)
+	{
+		temp = (LNode*)malloc(sizeof(LNode));
+		if (temp == NULL)
+			return FALSE;
+		temp->data = rand() % 100;
+		temp->next = (*head)->next;
+		(*head)->next = temp;
+		(*head)->data++;
+	}
+	return TRUE;
+}
+
+Bool ScanfPackage(ScanfQueue* queue, int num)
+{
+	if (queue == NULL)
+		return FALSE;
+	if (num<1 || num>MAXSIZE)
+		return FALSE;
+	for (int i = 0; i < num; i++)
+		scanf("%d", &(queue->data[i]));
+	queue->length = num;
+	return TRUE;
+}
+
+Bool HelpList()
+{
+	printf(" 1   初始线性表 2   销毁线性表\n");
+	printf(" 3   按位插入值 4   按位删除值\n");
+	printf(" 5   按值查找位 6   按位查找值\n");
+	printf(" 7   头插建立表 8   尾插建立表\n");
+	printf(" 101 打印线性表 102 随机初始化\n");
+	printf("-1   退出线性表 0   帮助线性表\n");
+	printf("-1   结束输入值\n");
+	return TRUE;
+}
---
+++ Data-Structure/SqList-LoopLink-Head/main.c
@@ -0,0 +1,223 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"root.h"
+//基本操作
+Bool InitList(LNode **);                //初始化线性表
+Bool DestoryList(LNode**);              //销毁线性表
+//常用操作
+Bool InsertTailList(LNode*);            //头插法建立线性表
+Bool InsertHeadList(LNode*);            //尾插法建立线性表
+Bool InsertElem(LNode*, int, EleType);  //按位插入元素
+Bool DeleteElem(LNode*, int);           //删除元素并返回值
+LNode* LocateElem(LNode*, EleType);     //按值查找元素
+LNode* GetElem(LNode*, int);            //按位查找元素
+//辅助操作
+Bool PrintList(LNode *);                //打印线性表
+Bool MenuList(LNode **,ScanfQueue *);   //菜单
+
+Bool InitList(LNode** head)
+{
+	if (head == NULL)
+		return FALSE;
+	(*head) = (LNode*)malloc(sizeof(LNode));
+	if (*head == NULL)
+		return FALSE;
+	(*head)->next = *head;
+	(*head)->data = 0;
+	return TRUE;
+}
+
+Bool DestroyList(LNode** head)
+{
+	if (head == NULL)
+		return FALSE;
+	LNode* tail = *head;
+	LNode* temp = tail->next;
+	for (; temp != *head; temp = tail)
+	{
+		tail = temp->next;
+		free(temp);
+	}
+	free(temp);
+	return TRUE;
+}
+
+Bool InsertHeadList(LNode* head)
+{
+	if (head == NULL)
+		return FALSE;
+	LNode* temp = NULL;
+	EleType data=0;
+	printf("Input Data:");
+	scanf("%d",&data);
+	while (data != -1)
+	{
+		temp = (LNode*)malloc(sizeof(LNode));
+		if (temp == NULL)
+			return FALSE;
+		temp->data = data;
+		temp->next = head->next;
+		head->next = temp;
+		head ->data++;
+		scanf("%d",&data);
+	}
+	return TRUE;
+}
+
+Bool InsertTailList(LNode* head)
+{
+	if (head == NULL)
+		return FALSE;
+	LNode* temp = NULL;
+	LNode* tail = head;
+	EleType data = 0;
+	printf("Input Data:");
+	scanf("%d", &data);
+	while (data != -1)
+	{
+		temp = (LNode*)malloc(sizeof(LNode));
+		if (temp == NULL)
+			return FALSE;
+		tail->next = temp;
+		temp->data = data;
+		temp->next = head;
+		tail = tail->next;
+		head->data++;
+		scanf("%d", &data);
+	}
+	return TRUE;
+}
+
+Bool InsertElem(LNode* head, int num, EleType data)
+{
+	if (head == NULL)
+		return FALSE;
+	if (num<1 || num>head->data)
+		return FALSE;
+	LNode* front = head;
+	LNode* temp = NULL;
+	for (int i = 1; i < num; i++, front = front->next);
+	temp = (LNode*)malloc(sizeof(LNode));
+	if (temp == NULL)
+		return FALSE;
+	temp->data = data;
+	temp->next = front->next;
+	front->next = temp;
+	return TRUE;
+}
+
+Bool DeleteElem(LNode* head, int num)
+{
+	if (head == NULL)
+		return FALSE;
+	if (num < 1 || num >head->data)
+		return FALSE;
+	LNode* front = head;
+	LNode* temp = NULL;
+	for (int i = 1; i < num; i++, front = front->next);
+	temp = front->next;
+	front->next = front->next->next;
+	free(temp);
+	head->data--;
+	return TRUE;
+}
+
+LNode* LocateElem(LNode* head, EleType data)
+{
+	if (head == NULL)
+		return FALSE;
+	LNode* temp = head->next;
+	for (; temp != head; temp = temp->next)
+		if (temp->data == data)
+			return temp;
+	return FALSE;
+}
+
+LNode* GetElem(LNode* head , int num)
+{
+	if (head == NULL)
+		return FALSE;
+	if (num<1 || num>head->data)
+		return FALSE;
+	LNode* temp = head;
+	for (int i = 1; i <= num; i++, temp = temp->next);
+	return temp;
+}
+
+Bool PrintList(LNode* head)
+{
+	if (head == NULL)
+		return FALSE;
+	LNode* temp = head->next;
+	for (; temp!= head; temp = temp->next)
+	{
+		printf("%2d ",temp->data);
+	}
+	return TRUE;
+}
+
+Bool MenuList(LNode** head, ScanfQueue* queue)
+{
+	while (TRUE) {
+		int num;
+		printf("Input Num:");
+		scanf("%d", &num);
+		switch (num) {
+		case 1: InitList(head); break;
+		case 2: DestroyList(head); break;
+		case 3:
+		{
+			printf("Input locate and data:");
+			ScanfPackage(queue, 2);
+			InsertElem(*head, queue->data[0], queue->data[1]);
+			break;
+		}
+		case 4:
+		{
+			printf("Input locate:");
+			ScanfPackage(queue, 1);
+			DeleteElem(*head, queue->data[0]);
+			break;
+		}
+		case 5:
+		{
+			printf("Input data:");
+			ScanfPackage(queue, 1);
+			printf("%p\n", LocateElem(*head, queue->data[0]));
+			break;
+		}
+		case 6:
+		{
+			printf("Input locate:");
+			ScanfPackage(queue, 1);
+			printf("%d\n", (GetElem(*head, queue->data[0]))->data);
+			break;
+		}
+		case 7:
+		{
+			InsertHeadList(*head);
+			break;
+		}
+		case 8:
+		{
+			InsertTailList(*head);
+			break;
+		}
+		case 101: PrintList(*head); break;
+		case 102: InitRandomList(head); break;
+		case 0: HelpList(); break;
+		case -1:return OK; break;
+		default:printf("Num Error!\n");
+		}
+		printf("\n");
+	}
+}
+
+int main(void)
+{
+	LNode* head = NULL;
+	ScanfQueue queue;
+	HelpList();
+	MenuList(&head,&queue);
+	return 0;
+}
